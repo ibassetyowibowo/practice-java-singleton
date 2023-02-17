0bc30a34bd0202a70462d2033c793e3102f5262d
Basuki Cahyo <basuki.wibowo@adira.co.id>
2023-02-18 06:17:39 +0700

    init project

---
+++ src/main/java/com/basuki/practicejavasingleton/PracticeJavaSingletonApplication.java
@@ -0,0 +1,13 @@
+package com.basuki.practicejavasingleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJavaSingletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJavaSingletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basuki/practicejavasingleton/PracticeJavaSingletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basuki.practicejavasingleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaSingletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
