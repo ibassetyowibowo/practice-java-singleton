09eaac79accf20a7b686b6d4b6f1ad9ad727ef5c
Basuki Cahyo <basuki.wibowo@adira.co.id>
2023-02-18 06:27:42 +0700

    init project

---
+++ src/main/java/basuki/designpatern/singleton/JavaDesignpaternSingletonApplication.java
@@ -0,0 +1,13 @@
+package basuki.designpatern.singleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignpaternSingletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignpaternSingletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basuki/designpatern/singleton/JavaDesignpaternSingletonApplicationTests.java
@@ -0,0 +1,13 @@
+package basuki.designpatern.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignpaternSingletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
